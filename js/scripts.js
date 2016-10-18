7cc70446f9253f1520ae128b4109983f9f2e722a
Ryan Mathisen and Tracie Weitzman <tracie@>
2016-10-18 15:44:26 -0700

    add additional Javascript relational alerts if cat or dog wins

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
+var meowCount = 0;
+var barkCount = 0;
+
 $(document).ready(function() {
   $("button#sillycat").click(function() {
     $("ul.cat").append("<li>Meow!</li>");
-  
+    meowCount++;
+    if (meowCount === 5) {
+      alert("Cat wins!");
+    }
   });
 
   $("button#sillydog").click(function() {
     $("ul.dog").append("<li>Bark!</li>");
-
+    barkCount++;
+    if (barkCount === 5) {
+      alert("Dog wins!");
+    }
   });
 
 });
